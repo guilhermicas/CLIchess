d1e66a1d4371d163e0a22829c86d7258a65f196c
Guilherme Silva <sotrabalhosgui@hotmail.com>
2021-04-28 13:45:11 +0000

    Brainstorming

--- c.c
+++ c.c
@@ -2,25 +2,36 @@
 #include <stdbool.h> 
 
 //Game Board (where the magic happens)
-char board[] = "RNBQKBNRPPPPPPPP                                RNBQKBNRPPPPPPPP";
+char board[] = "RNBQKBNRPPPPPPPP                                PPPPPPPPRNBQKBNR";
 int board_size = (sizeof board) - 1;
 
 //0 = White, 1 = Black
 bool turn = 0;
 
 void renderBoard(){
+  int currRank = 8;
+
+  printf("%d ", currRank);
+  currRank -= 1;
   for(int i = 0; i < board_size; i++){
     printf("%c",board[i]);
 
     if(((i+1) % 8) == 0){
       printf("\n");
+      if(currRank != 0){
+        printf("%d ", currRank);
+        currRank -= 1;
+      }
     }
   }
+
+  printf("\n  abcdefgh\n");
 }
 
 int main(int argc, char **argv[]){
   renderBoard();
 
+  //getMove();
   return 1;
 }
 
@@ -110,6 +121,42 @@ Game system:
 
       historyLog():
         creates a file with the history of the ended game
+
+int CCNToIdx(CCN){
+  //FileChar tem de ser lowercase
+  return ((FILEChar - 140) - 1) + (8 * (8 - RANKNumber));
+}
+Ex:
+c = 143
+CCN = Nc3
+Idx = 42
+Idx = ((c - 140) - 1) + (8 * (8 - 3))
+Idx = 2 + 40 = 42
+
+Now Based on the chess piece, make some arithmetic to determine if there is any chess piece nearby that can
+make the move
+for example in Nc3 we need to check if there is a knight of the !Turn's COLOR! in:
+  CCN = Nc3
+  Idx = 42
+  -a2(48)
+  -a4(32)
+  -b5(25)
+  -d5(27)
+  -e4(36)
+  -e2(52)
+  -d1(59)
+  -b1(57)
+
+  -b5(25) = Idx - 17
+  -d5(27) = Idx - 15
+  -a4(32) = Idx - 10
+  -e4(36) = Idx - 6
+  -a2(48) = Idx + 6
+  -e2(52) = Idx + 10
+  -b1(57) = Idx + 15
+  -d1(59) = Idx + 17
+
+we also have to check if its out of bounds
 
 Move(newPosition(CCN notation)):
     newPosition is given inputed by the user
